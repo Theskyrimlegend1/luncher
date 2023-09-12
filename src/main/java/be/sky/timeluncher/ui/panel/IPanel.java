3cf33a73d228a4af491bc93f188d154a0dada86f
Nathan <superskyrim2.0@gmail.com>
2023-09-12 19:33:40 +0200

    init

---
+++ src/main/java/be/sky/timeluncher/ui/panel/IPanel.java
@@ -0,0 +1,12 @@
+package be.sky.timeluncher.ui.panel;
+
+import be.sky.timeluncher.ui.PanelManager;
+import javafx.scene.layout.GridPane;
+
+public interface IPanel {
+    void init(PanelManager panelManager);
+    GridPane getLayout();
+    void onShow();
+    String getName();
+    String getStylesheetPath();
+}
